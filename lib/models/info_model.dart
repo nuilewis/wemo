c2c4e67f6ca610028afacc6e10457d0235091459
nuilewis <lewisnuikweh@gmail.com>
2022-05-02 11:34:59 +0100

    created data models

---
+++ lib/models/info_model.dart
@@ -0,0 +1,8 @@
+
+class NumberInfo {
+
+  final int number;
+ final String name;
+
+  NumberInfo({required this.number, required this.name});
+}
---
+++ lib/models/transaction_history.dart
@@ -0,0 +1,15 @@
+class Transaction {
+  final String name;
+ final  int number;
+  final String network;
+ final  DateTime time;
+
+
+  Transaction({required this.name, required this.number, required this.network, required this.time});
+
+}
+
+enum TransactionType{
+  transactionType.send;
+  transactionType.recived;
+}
