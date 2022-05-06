9a69f70ae7d4a4b57072fa60d82aa8050cee75b2
nuilewis <lewisnuikweh@gmail.com>
2022-05-06 23:22:46 +0100

    edited screens

--- lib/screens/homescreen/homescreen.dart
+++ lib/screens/homescreen/homescreen.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:qr_flutter/qr_flutter.dart';
+import 'package:provider/provider.dart';
+
 import 'package:wemo/constants.dart';
+import 'package:wemo/providers/number_provider.dart';
 import 'package:wemo/screens/scanscreen/scan_screen.dart';
 import 'package:wemo/services/ussd_service.dart';
 
 import '../recievescreen/components/showQR.dart';
 
 class HomeScreen extends StatefulWidget {
-  static const id = "homesceen";
+  static const id = "homescreen";
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
@@ -19,42 +20,48 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-          elevation: 0,
-          child: const Icon(
-            Icons.call,
-            color: Colors.white,
-          ),
-          onPressed: () {
-            WemoUSSDService().requestUSSD("a", "a", "a");
-          }),
-      body: SingleChildScrollView(
-        child: (Column(
-          children: [
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Container(
-                height: 100,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(24),
-                    gradient: const LinearGradient(
-                        colors: [kPurple, kPurple60],
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomRight)),
-                width: screenSize.width * .7,
+    return Consumer<MomoNumberData>(builder: (context, momoNumData, child) {
+      return Scaffold(
+        floatingActionButton: FloatingActionButton(
+            elevation: 0,
+            child: const Icon(
+              Icons.call,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              WemoUSSDService().requestUSSD("a", "a", "a");
+            }),
+        body: SingleChildScrollView(
+          child: (Column(
+            children: [
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Container(
+                  height: 250,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(24),
+                      gradient: const LinearGradient(
+                          colors: [kPurple, kPurple60],
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight)),
+                  width: screenSize.width * .8,
+                ),
               ),
-            ),
-            ShowQRCode(
-              data: "hey guys this si austin",
-            ),
-            ElevatedButton(onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context)=> ScanScreen()));
-            }, child: Text("ScanQr"))
-          ],
-        )),
-      ),
-    );
+              ShowQRCode(
+                data: "hey guys this si austin",
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const ScanScreen()));
+                  },
+                  child: const Text("ScanQr"))
+            ],
+          )),
+        ),
+      );
+    });
   }
 }
-
--- lib/screens/scanscreen/scan_screen.dart
+++ lib/screens/scanscreen/scan_screen.dart
@@ -4,80 +4,68 @@ import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'package:wemo/constants.dart';
 
-
 class ScanScreen extends StatefulWidget {
-  const ScanScreen({ Key? key }) : super(key: key);
+  const ScanScreen({Key? key}) : super(key: key);
 
   @override
   State<ScanScreen> createState() => _ScanScreenState();
 }
 
 class _ScanScreenState extends State<ScanScreen> {
+  Barcode? result;
+  QRViewController? qrViewController;
+  final GlobalKey qrKey = GlobalKey(debugLabel: "QR");
 
-Barcode? result;
-QRViewController? qrViewController;
-final GlobalKey qrKey = GlobalKey(debugLabel: "QR");
+  void _onQRViewCreated(QRViewController qrViewController) {
+    setState(() {
+      this.qrViewController = qrViewController;
+    });
 
-
-
-void _onQRViewCreated( QRViewController qrViewController){
-
-setState(() {
-  this.qrViewController = qrViewController;
-
-});
-
-qrViewController.scannedDataStream.listen((scanData) {
-  
-  setState(() {
-    result = scanData;
-  });
-});
-
-}
-
+    qrViewController.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData;
+      });
+      print("scan data result is ${result?.code}");
+    });
+  }
 
 //We have to paus the camera so hot reload can work
-@override
-
-void reassemble() {
-    
+  @override
+  void reassemble() {
     super.reassemble();
-    if(Platform.isAndroid){
+    if (Platform.isAndroid) {
       qrViewController!.pauseCamera();
-    }else if(Platform.isIOS){
+    } else if (Platform.isIOS) {
       qrViewController!.resumeCamera();
     }
   }
 
-
   void readQr() async {
-
-    if(result!=null){
+    if (result != null) {
       qrViewController!.pauseCamera();
-      print(result!.code);
+      debugPrint(result!.code);
       qrViewController!.dispose();
     }
   }
 
   @override
   void dispose() {
-  
     super.dispose();
     qrViewController?.dispose();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: QRView(key: qrKey,
-      onQRViewCreated: _onQRViewCreated,
-      overlay: QrScannerOverlayShape(borderColor: kPurple,
-      borderRadius: 28,
-      borderLength: 30,
-      borderWidth: 10,
-      cutOutSize:  250),
+      body: QRView(
+        key: qrKey,
+        onQRViewCreated: _onQRViewCreated,
+        overlay: QrScannerOverlayShape(
+            borderColor: kPurple,
+            borderRadius: 28,
+            borderLength: 30,
+            borderWidth: 10,
+            cutOutSize: 250),
       ),
     );
   }
