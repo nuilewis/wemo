2a0e0e5f15a8e2d43d40d058dd7e1a1a2d79dc3b
nuilewis <lewisnuikweh@gmail.com>
2022-05-08 15:48:34 +0100

    made custom phone number card widget

---
+++ lib/screens/homescreen/components/phone_num_card.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+import '../../../constants.dart';
+
+
+class PhoneNumberCard extends StatelessWidget {
+  const PhoneNumberCard({
+    Key? key,
+    required this.number,
+    required this.name,
+    required this.network,
+  }) : super(key: key);
+
+  final String number;
+  final String name;
+  final String network;
+
+  @override
+  Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
+    return Container(
+      margin: EdgeInsets.only(right: kDefaultPadding),
+      height: 120,
+      decoration: BoxDecoration(
+          boxShadow:  const [
+             BoxShadow(
+              color: kPurple20,
+              offset: Offset(5, 10),
+              blurRadius: 20,
+              spreadRadius: 8,
+            ),
+          ],
+          borderRadius: BorderRadius.circular(kDefaultPadding2x),
+          gradient: const LinearGradient(
+              colors: [kPurple, kPurple20],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight)),
+      width: screenSize.width * .8,
+      padding: const EdgeInsets.all(kDefaultPadding),
+      alignment: Alignment.topLeft,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            number,
+            style: Theme.of(context)
+                .textTheme
+                .headline1!
+                .copyWith(color: Colors.white),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          Text(
+            name,
+            style: Theme.of(context)
+                .textTheme
+                .bodyText1!
+                .copyWith(color: Colors.white),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          Text(
+            network,
+            style: Theme.of(context)
+                .textTheme
+                .bodyText2!
+                .copyWith(color: Colors.white),
+          ),
+        ],
+      ),
+    );
+  }
+}
