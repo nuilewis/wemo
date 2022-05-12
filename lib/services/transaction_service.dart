cac062cd66cd9010b3bae2baeb69133d03eea958
nuilewis <lewisnuikweh@gmail.com>
2022-05-12 12:42:11 +0100

    worked on transaction service

--- lib/services/transaction_service.dart
+++ lib/services/transaction_service.dart
@@ -1,41 +1,32 @@
-
 import 'package:wemo/enums/wemo_enums.dart';
 
-class TransactionService{
+class TransactionService {
+  static NetworkType determinNetwork(int phoneNumber) {
+    NetworkType networkType = NetworkType.mtn;
 
- static Network determinNetwork(int phoneNumber){
+    ///takes the phone number and returns the network
+    ///but befor that, first remove any 237 in the number before making checking
 
- Network networkType = Network.mtn;
-   ///takes the phone number and returns the network
-   ///but befor that, first remove any 237 in the number before making checking
-   
+    ///Removing '237's
+    ///
 
-   ///Removing '237's
-   ///
-   
-  String phoneNumToString = phoneNumber.toString();
-   
-   if(phoneNumToString.startsWith("237") ){
+    String phoneNumToString = phoneNumber.toString();
 
-     phoneNumToString = phoneNumToString.substring(3);
-   } else if(phoneNumToString.startsWith("+237")){
-     
-     phoneNumToString = phoneNumToString.substring(4);
-   }
-   
-   
-  
+    if (phoneNumToString.startsWith("237")) {
+      phoneNumToString = phoneNumToString.substring(3);
+    } else if (phoneNumToString.startsWith("+237")) {
+      phoneNumToString = phoneNumToString.substring(4);
+    }
 
-   if(phoneNumToString.startsWith("67") || phoneNumToString.startsWith("68") || phoneNumToString.startsWith("65")){
-    networkType = Network.mtn;
-   } else if(phoneNumToString.startsWith("69") || phoneNumToString.startsWith("65")  ){
-     networkType = Network.orange;
-   }
+    if (phoneNumToString.startsWith("67") ||
+        phoneNumToString.startsWith("68") ||
+        phoneNumToString.startsWith("65")) {
+      networkType = NetworkType.mtn;
+    } else if (phoneNumToString.startsWith("69") ||
+        phoneNumToString.startsWith("65")) {
+      networkType = NetworkType.orange;
+    }
 
-
-   return networkType;
- }
-
-
-
+    return networkType;
+  }
 }
