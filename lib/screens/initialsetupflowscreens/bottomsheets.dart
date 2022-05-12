e156a1b24bd48e583255616bef5145e732b35ec7
nuilewis <lewisnuikweh@gmail.com>
2022-05-12 12:36:55 +0100

    made bottomsheet to hold pageview

--- lib/screens/initialsetupflowscreens/bottomsheets.dart
+++ lib/screens/initialsetupflowscreens/bottomsheets.dart
@@ -5,22 +5,35 @@ import 'package:wemo/screens/initialsetupflowscreens/verify_num_screen.dart';
 import 'show_qr_screen.dart';
 
 class WemoBottomSheets extends StatefulWidget {
-  const WemoBottomSheets({ Key? key }) : super(key: key);
+  const WemoBottomSheets({Key? key}) : super(key: key);
 
   @override
   State<WemoBottomSheets> createState() => _WemoBottomSheetsState();
 }
 
 class _WemoBottomSheetsState extends State<WemoBottomSheets> {
-
+  PageController pageController = PageController();
   int currentView = 0;
-  List <Widget> pages = [
+  List<Widget> pages = [
     const VerifyNumberScreen(),
     const VerificationSuccessScreen(),
     const ShowQRCodeScreen(),
   ];
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return pages[currentView];
+    return Container(
+      child: PageView.builder(
+        itemBuilder: (context, index) {
+          return pages[index];
+        },
+      ),
+    );
   }
 }
