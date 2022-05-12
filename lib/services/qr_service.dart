2a749ee0c72c66551145f303b794382dbacbbdbf
nuilewis <lewisnuikweh@gmail.com>
2022-05-12 12:42:27 +0100

    adjusted qr service

--- lib/services/qr_service.dart
+++ lib/services/qr_service.dart
@@ -1,7 +1,9 @@
-import 'package:flutter/foundation.dart';
+
 
 class QrService {
   ///Interpolate name and number into a single string to feed to tthe GR generator
+
+  ///INterpolating the Name and nUmber Strings
 
   String joinNameNUmber({required int number, required String name}) {
     String numToString = number.toString();
