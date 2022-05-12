cecb4f0da7b8fd0221679f8b4eab9032b44ba2c0
nuilewis <lewisnuikweh@gmail.com>
2022-05-12 12:34:42 +0100

    created sendmoney model

---
+++ lib/models/send_money_model.dart
@@ -0,0 +1,14 @@
+
+
+class SendMoney {
+
+
+int amount;
+String pin;
+String name;
+String number;
+
+SendMoney({required this.amount, required this.pin, required this.name, required this.number});
+
+
+}
