df51d68e720e8dbda25fbafba6a0e3dc3ef1539d
nuilewis <lewisnuikweh@gmail.com>
2022-05-14 22:39:39 +0100

    added listview and pageview for numbers

--- lib/screens/homescreen/homescreen.dart
+++ lib/screens/homescreen/homescreen.dart
@@ -1,16 +1,18 @@
+import 'package:expandable_page_view/expandable_page_view.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
-
 import 'package:wemo/constants.dart';
-import 'package:wemo/global_components/wemo_button_small.dart';
+import 'package:wemo/global_components/wemo_button.dart';
+import 'package:wemo/models/transaction_model.dart';
 import 'package:wemo/providers/momo_num_provider.dart';
 import 'package:wemo/providers/transaction_provider.dart';
+import 'package:wemo/screens/homescreen/components/transaction_card.dart';
 import 'package:wemo/screens/initialsetupflowscreens/addnumberscreen.dart';
 import 'package:wemo/screens/initialsetupflowscreens/show_qr_screen.dart';
 import 'package:wemo/screens/scanscreen/scan_screen.dart';
 import '../../models/momo_num_model.dart';
-
 import 'components/add_number_button.dart';
 import 'components/phone_num_card.dart';
 
@@ -24,6 +26,18 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int currentIndex = 0;
+  final GlobalKey<FormState> addNumberFormKey = GlobalKey<FormState>();
+  final Key addNumberNumkey = GlobalKey();
+  final Key addNumberNameKey = GlobalKey();
+
+  final PageController pageController = PageController(viewportFraction: .8);
+
+  @override
+  void dispose() {
+    super.dispose();
+    pageController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
@@ -39,81 +53,142 @@ class _HomeScreenState extends State<HomeScreen> {
                 // SizedBox(
                 //   height: screenSize.height,
                 // ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const SizedBox(
-                      height: kDefaultPadding2x * 3,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: kDefaultPadding2x),
-                      child: Text(
-                        "Welcome Back!",
-                        style: Theme.of(context).textTheme.headline1,
+                SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const SizedBox(
+                        height: kDefaultPadding2x * 2,
                       ),
-                    ),
-
-                    ///Phone Numbers
-                    SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          const SizedBox(
-                            width: kDefaultPadding,
-                            height: 190,
-                          ),
-                          SizedBox(
-                            ///Use the sizebox to contrain the height of the List view
-
-                            height: 120,
-                            child: ListView.builder(
-                              physics: const NeverScrollableScrollPhysics(),
-                              scrollDirection: Axis.horizontal,
-                              itemCount: momoNumData.momoNumberList.length,
-                              shrinkWrap: true,
-                              itemExtent: screenSize.width * .75,
-                              itemBuilder: (context, index) {
-                                ///Setting the current index so that
-                                currentIndex = index;
-                                print(currentIndex);
-
-                                List<MomoNumber> momoNumbersList =
-                                    momoNumData.momoNumberList;
-
-                                return PhoneNumberCard(
-                                    number: momoNumbersList[index]
-                                        .number
-                                        .toString(),
-                                    name: momoNumbersList[index].name,
-                                    network: momoNumbersList[index]
-                                        .network
-                                        .toString()
-                                        .toUpperCase());
-                              },
-                            ),
-                          ),
-                          AddNumberButton(
-                            onPressed: () {
-                              HapticFeedback.lightImpact();
-                              Feedback.forTap(context);
-
-                              Navigator.pushNamed(context, AddNumberScreen.id);
-                            },
-                          ),
-                        ],
+                      Padding(
+                        padding: const EdgeInsets.only(left: kDefaultPadding2x),
+                        child: Text(
+                          "Welcome Back!",
+                          style: Theme.of(context).textTheme.headline1,
+                        ),
                       ),
-                    ),
 
-                    Padding(
-                      padding: const EdgeInsets.only(left: kDefaultPadding2x),
-                      child: Text(
-                        "History",
-                        style: Theme.of(context).textTheme.headline1,
+                      SizedBox(
+                        height: 190,
+                        width: screenSize.width,
+                        child: PageView.builder(
+                          //padEnds: false,
+                          controller: pageController,
+                          //physics:  NeverScrollableScrollPhysics(),
+                          scrollDirection: Axis.horizontal,
+                          itemCount: momoNumData.momoNumberList.length,
+                          onPageChanged: (index) {
+                            ///Setting the current index so that
+                            setState(() {
+                              currentIndex = index;
+                            });
+                            print("current index is $currentIndex");
+                          },
+                          itemBuilder: (context, index) {
+                            List<MomoNumber> momoNumbersList =
+                                momoNumData.momoNumberList;
+
+                            return Center(
+                              child: PhoneNumberCard(
+                                  number:
+                                      momoNumbersList[index].number.toString(),
+                                  name: momoNumbersList[index].name,
+                                  network: momoNumbersList[index]
+                                      .network
+                                      .toString() //convert network type to string
+                                      .substring(
+                                          12) //remove "NetworkType." from converted string to get actual network
+                                      .toUpperCase()),
+                            );
+                          },
+                        ),
                       ),
-                    ),
-                  ],
+
+                      ///Phone Numbers
+                      AddNumberButton(
+                        onPressed: () {
+                          HapticFeedback.lightImpact();
+                          Feedback.forTap(context);
+
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => AddNumberScreen(
+                                      formkey: addNumberFormKey,
+                                      nameKey: addNumberNameKey,
+                                      numberKey: addNumberNumkey,
+                                      isCalledFromHomeScreen: true)));
+                        },
+                      ),
+
+                      ///History
+                      Padding(
+                        padding: const EdgeInsets.only(left: kDefaultPadding2x),
+                        child: Text(
+                          "History",
+                          style: Theme.of(context).textTheme.headline1,
+                        ),
+                      ),
+
+                      const SizedBox(
+                        height: kDefaultPadding2x,
+                      ),
+                      transactionData.transactionsList.isEmpty
+                          ? Center(
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: kDefaultPadding2x),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      "Nothing to see here",
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .headline1!
+                                          .copyWith(color: kPurple),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                    const SizedBox(
+                                      height: kDefaultPadding,
+                                    ),
+                                    Text(
+                                      "You haven't done any transactions with this number, once you perform a transaction, it'll appear here.",
+                                      style:
+                                          Theme.of(context).textTheme.bodyText2,
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            )
+                          : Expanded(
+                              child: ListView.builder(
+                                  itemCount:
+                                      transactionData.transactionsList.length,
+                                  itemBuilder: (context, index) {
+                                    List<Transaction> transactionList =
+                                        transactionData.transactionsList;
+
+                                    return TransactionCard(
+                                        amount: transactionList[index]
+                                            .amount
+                                            .toString(),
+                                        number: transactionList[index]
+                                            .number
+                                            .toString(),
+                                        name: transactionList[index]
+                                            .name
+                                            .toString(),
+                                        time: transactionList[index].time,
+                                        transactionType: transactionList[index]
+                                            .transactionType);
+                                  }),
+                            )
+                    ],
+                  ),
                 ),
                 Padding(
                   padding: const EdgeInsets.only(
@@ -126,7 +201,8 @@ class _HomeScreenState extends State<HomeScreen> {
                       mainAxisSize: MainAxisSize.min,
                       children: [
                         Expanded(
-                          child: WemoButtonSmall(
+                          child: WemoButton(
+                            isSmall: true,
                             textColor: Colors.white,
                             title: "Send",
                             showIcon: true,
@@ -142,7 +218,8 @@ class _HomeScreenState extends State<HomeScreen> {
                           width: kDefaultPadding,
                         ),
                         Expanded(
-                          child: WemoButtonSmall(
+                          child: WemoButton(
+                            isSmall: true,
                             textColor: Colors.white,
                             title: "Receive",
                             showIcon: true,
@@ -154,8 +231,23 @@ class _HomeScreenState extends State<HomeScreen> {
                                   isScrollControlled: true,
                                   isDismissible: true,
                                   context: context,
-                                  builder: (context) =>
-                                      ShowQRCodeScreen(index: currentIndex));
+                                  builder: (context) {
+                                    return Container(
+                                      decoration: BoxDecoration(
+                                        color: Theme.of(context)
+                                            .scaffoldBackgroundColor,
+                                        borderRadius: const BorderRadius.only(
+                                          topRight: Radius.circular(
+                                              kDefaultPadding2x),
+                                          topLeft: Radius.circular(
+                                              kDefaultPadding2x),
+                                        ),
+                                      ),
+                                      child: ShowQRCodeScreen(
+                                        index: currentIndex,
+                                      ),
+                                    );
+                                  });
                             },
                             iconLink: "assets/svg/received_icon.svg",
                           ),
