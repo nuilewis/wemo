d53a7f2766c0a54d68eaabbc14db867d731de08d
nuilewis <lewisnuikweh@gmail.com>
2022-05-19 20:22:34 +0100

    added transaction consumer

--- lib/screens/sendscreen/confrim_and_send_dialog.dart
+++ lib/screens/sendscreen/confrim_and_send_dialog.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+import 'package:wemo/enums/wemo_enums.dart';
 import 'package:wemo/global_components/wemo_button.dart';
+import 'package:wemo/models/transaction_model.dart';
+import 'package:wemo/providers/transaction_provider.dart';
+import 'package:wemo/services/transaction_service.dart';
 import 'package:wemo/services/ussd_service.dart';
 
 import '../../constants.dart';
@@ -16,8 +20,8 @@ class ConfirmAndSendDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Size screenSize = MediaQuery.of(context).size;
-    return Consumer<SendMoneyData>(
-      builder: (context, sendMoneyData, child) {
+    return Consumer2<SendMoneyData, TransactionData>(
+      builder: (context, sendMoneyData, transactionData, child) {
         return Padding(
           padding: const EdgeInsets.all(kDefaultPadding),
           child: Stack(
@@ -53,7 +57,8 @@ class ConfirmAndSendDialog extends StatelessWidget {
                                 .bodyText1!
                                 .copyWith(fontSize: 20)),
                         TextSpan(
-                            text: " ${sendMoneyData.moneyToSend?.name}",
+                            text:
+                                " ${sendMoneyData.moneyToSend?.name == "" ? sendMoneyData.moneyToSend?.number : sendMoneyData.moneyToSend?.name}",
                             style: Theme.of(context)
                                 .textTheme
                                 .bodyText1!
@@ -87,10 +92,32 @@ class ConfirmAndSendDialog extends StatelessWidget {
                       //   amount: sendMoneyData.moneyToSend!.amount,
                       // );
                       await WemoUSSDService().requestMtnMomo(
+                        context,
                         number: sendMoneyData.moneyToSend!.number,
                         pin: sendMoneyData.moneyToSend!.pin,
                         amount: sendMoneyData.moneyToSend!.amount,
                       );
+
+                      if (transactionData.transactionResult != null) {
+                        return;
+                      } else if (transactionData.transactionResult!
+                          .contains("succesful transfer")) {
+                        ///Add a new transaction if the transaction is successful
+                        transactionData.addTransaction(
+                          Transaction(
+                            charges: TransactionService.calculateCharges(
+                                    sendMoneyData.moneyToSend!.amount) -
+                                sendMoneyData.moneyToSend!.amount,
+                            name: sendMoneyData.moneyToSend!.name,
+                            network: TransactionService.determinNetwork(
+                                sendMoneyData.moneyToSend!.number),
+                            transactionType: TransactionType.sent,
+                            number: sendMoneyData.moneyToSend!.number,
+                            time: DateTime.now(),
+                            amount: sendMoneyData.moneyToSend!.amount,
+                          ),
+                        );
+                      }
                     },
                   ),
                   const SizedBox(
