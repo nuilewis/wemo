2241f34605d90b5ffc93f6df01e481c4ae6b8408
nuilewis <lewisnuikweh@gmail.com>
2022-05-19 23:13:49 +0100

    added shared prefs and saved objects to disk with shared prefs

--- lib/models/momo_num_model.dart
+++ lib/models/momo_num_model.dart
@@ -6,4 +6,15 @@ class MomoNumber {
   final NetworkType network;
 
   MomoNumber({required this.number, required this.name, required this.network});
+
+  ///Assigning variables values to the map in their corresponding map keys
+  MomoNumber.fromMap(Map map)
+      : number = map["number"],
+        name = map["name"],
+        network = map["network"];
+
+  ///Converting from MomoNumber object to a map
+  Map toMap() {
+    return {"number": number, "name": name, "network": network};
+  }
 }
--- lib/providers/momo_num_provider.dart
+++ lib/providers/momo_num_provider.dart
@@ -1,22 +1,44 @@
 import 'package:flutter/cupertino.dart';
 import 'package:wemo/models/momo_num_model.dart';
+import 'package:wemo/services/shared_prefs/shared_prefs_methods.dart';
 import 'package:wemo/services/transaction_service.dart';
 
 class MomoNumberData extends ChangeNotifier {
   List<MomoNumber> momoNumberList = [];
 
-  void addMomoNUmber({required String name, required String number}) {
-    momoNumberList.add(MomoNumber(
+  ///Get saved numbers and assign to momo Numbers
+
+  void getSavedMomoNUmber() async {
+    SharedPrefsService().initSharedPrefs();
+
+    List<MomoNumber> savedMomoNumber = SharedPrefsService().savedNumbersList;
+
+    if (savedMomoNumber.isNotEmpty) {
+      momoNumberList = SharedPrefsService().savedNumbersList;
+    }
+    notifyListeners();
+  }
+
+  void addMomoNumber({required String name, required String number}) {
+    MomoNumber momoNumberToAddAndSave = MomoNumber(
         name: name,
         number: number,
-        network: TransactionService.determinNetwork(number)));
+        network: TransactionService.determinNetwork(number));
+    momoNumberList.add(momoNumberToAddAndSave);
 
-    print("momoList is $momoNumberList");
+    //also add to saved momo numbers
+    SharedPrefsService()
+        .addToMomoSavedList(numberToSave: momoNumberToAddAndSave);
+
+    debugPrint("momoList is $momoNumberList");
     notifyListeners();
   }
 
   void deleteMomoNUmber(int index) {
     momoNumberList.removeAt(index);
+
+    //also delete momoNumber from saved List
+    SharedPrefsService().removeMomoNumFromSavedList(index);
     notifyListeners();
   }
 }
