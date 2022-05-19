a654ef66dfcdeab9eabb82398041b420bdd91afb
nuilewis <lewisnuikweh@gmail.com>
2022-05-19 20:26:49 +0100

    added Wemo message

--- lib/services/ussd_service.dart
+++ lib/services/ussd_service.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:ussd_advanced/ussd_advanced.dart';
+import 'package:wemo/providers/transaction_provider.dart';
 
 class WemoUSSDService {
   requestUSSD(
@@ -24,22 +26,26 @@ class WemoUSSDService {
   }
 
   requestMtnMomo(
-      {required String number,
-      required String pin,
-      required int amount}) async {
+    BuildContext context, {
+    required String number,
+    required String pin,
+    required int amount,
+  }) async {
     int subscriptionId = -1;
     //sim 1 or 2 and -1 is for the default phone settings
 
-    String mtnMomoCode = "*126*1*1*$number*$amount*ref*$pin#";
-    String mtnMomoCodeNoPin = "*126*1*1*$number*$amount*ref*$pin#";
+    String mtnMomoCode = "*126*1*1*$number*$amount*Transferred with Wemo*$pin#";
+    String mtnMomoCodeNoPin = "*126*1*1*$number*$amount*Transferred with Wemo#";
     String orangeMomoCode = "#150*1*1*$number*$amount*$pin";
+    String orangeMomoCodeNoPin = "#150*1*1*$number*$amount#";
     try {
       String? response = await UssdAdvanced.multisessionUssd(
           code: mtnMomoCodeNoPin, subscriptionId: subscriptionId);
       debugPrint("response 1 is : $response");
 
-      String? res2 = await UssdAdvanced.sendMessage("1");
-      debugPrint("response 2 is $res2");
+      Provider.of<TransactionData>(context).setTransactionResult(response);
+      // String? res2 = await UssdAdvanced.sendMessage("1");
+      // debugPrint("response 2 is $res2");
     } catch (e) {
       debugPrint("multisession USSD error is $e");
       //throw e;
