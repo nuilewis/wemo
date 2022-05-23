0a8846b42d31e2a2ae0d08dee622b0d87fee5aec
nuilewis <lewisnuikweh@gmail.com>
2022-05-23 22:55:05 +0100

    initialised shared prefs

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:wemo/providers/momo_num_provider.dart';
 import 'package:wemo/providers/send_money_provider.dart';
 import 'package:wemo/providers/transaction_provider.dart';
 import 'package:wemo/screens/homescreen/homescreen.dart';
 import 'package:wemo/screens/initialsetupflowscreens/addnumberscreen.dart';
-import 'package:wemo/screens/rootScreen/root_screen.dart';
 import 'package:wemo/screens/scanscreen/scan_screen.dart';
-import 'package:wemo/screens/onboardingscreen//onboarding_screen.dart';
+import 'package:wemo/screens/onboardingscreen/onboarding_screen.dart';
+import 'package:wemo/services/shared_prefs/shared_prefs_methods.dart';
 import 'package:wemo/theme.dart';
 
-void main() {
+int? startupCount;
+String startupCountKey = "startup_count";
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  wemoSharedPrefs = await SharedPreferences.getInstance();
+  loadMomoSPData();
+  loadTransactionSPData();
+  startupCount = wemoSharedPrefs.getInt(startupCountKey);
+  await wemoSharedPrefs.setInt(startupCountKey, 1);
+
   runApp(const MyApp());
 }
 
@@ -34,7 +44,10 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         theme: wemoLightTheme(context),
         //darkTheme: wemoDarkTheme(context),
-        home: const RootScreen(),
+        initialRoute: startupCount == 0 || startupCount == null
+            ? OnboardingScreen.id
+            : HomeScreen.id,
+
         routes: {
           HomeScreen.id: (context) => const HomeScreen(),
           OnboardingScreen.id: (context) => const OnboardingScreen(),
