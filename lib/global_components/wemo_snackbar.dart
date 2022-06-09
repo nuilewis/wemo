db34951ac08fed8381a3a6f735fd601288fb0168
nuilewis <lewisnuikweh@gmail.com>
2022-06-09 20:06:57 +0100

    added snack bar

---
+++ lib/global_components/wemo_snackbar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+
+void wemoSnackBar(BuildContext context,
+    {required String message, required bool success}) {
+  final customSnackBar = SnackBar(
+    behavior: SnackBarBehavior.floating,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(20),
+    ),
+    elevation: 20,
+    margin: const EdgeInsets.symmetric(
+        vertical: kDefaultPadding2x, horizontal: kDefaultPadding),
+    padding: const EdgeInsets.symmetric(
+        vertical: kDefaultPadding2x, horizontal: kDefaultPadding),
+    backgroundColor: success ? Colors.white : kFuchsia,
+    content: Text(
+      message,
+      style: success
+          ? Theme.of(context).textTheme.bodyText2
+          : Theme.of(context)
+              .textTheme
+              .bodyText1!
+              .copyWith(color: Colors.white),
+    ),
+  );
+  ScaffoldMessenger.of(context).showSnackBar(customSnackBar);
+}
