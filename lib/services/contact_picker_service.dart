59101c824f8b8a87cfb0cbf0e3d75288fb2c9d12
nuilewis <lewisnuikweh@gmail.com>
2022-06-11 23:08:04 +0100

    optimzed contact picker to make it more robust, removed non number characters and shortened long numbers

--- lib/services/contact_picker_service.dart
+++ lib/services/contact_picker_service.dart
@@ -3,14 +3,31 @@ import 'package:fluttercontactpicker/fluttercontactpicker.dart';
 class ContactPickerService {
   Future<Map<String, String?>> pickContact() async {
     final PhoneContact contact = await FlutterContactPicker.pickPhoneContact();
-    String? pickedNumber = contact.phoneNumber?.number;
-    String? pickedName = contact.fullName;
+    String? number = contact.phoneNumber?.number;
+    String? name = contact.fullName;
+    Map<String, String?> fullContact = {};
 
-    ///Arranging the pciked name and number into a Map
-    Map<String, String?> fullContact = {
-      "number": pickedNumber,
-      "name": pickedName
-    };
+    ///Remove uneccasry srting from the number
+    ///
+    String optimizedNum = number!.replaceAll(RegExp("[^0-9]"), "");
+
+    ///Check if number is longer than 9 digits, if so, cut it down
+
+    if (optimizedNum.length > 9) {
+      String shortenedOptimizedNumber =
+          optimizedNum.substring(optimizedNum.length - 9);
+      fullContact = {
+        "number": shortenedOptimizedNumber,
+        "name": name,
+      };
+    } else {
+      ///Arranging the picked name and number into a Map
+      fullContact = {
+        "number": optimizedNum,
+        "name": name,
+      };
+    }
+
     return fullContact;
   }
 }
