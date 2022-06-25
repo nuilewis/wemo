9f547c7076acdcc543f775dcfb5596aaf62e054a
nuilewis <lewisnuikweh@gmail.com>
2022-06-25 15:37:14 +0100

    worked on pdf saving jank

--- lib/services/qr_service.dart
+++ lib/services/qr_service.dart
@@ -1,5 +1,4 @@
 import 'dart:typed_data';
-import 'dart:ui';
 import 'package:flutter/foundation.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:wemo/constants.dart';
@@ -18,43 +17,66 @@ class QrService {
   }
 
   ///Create and Save a QR Image
-
-  Future<Uint8List?> createQRImage(String data) async {
-    final qrValidationResult = QrValidator.validate(
-        data: data,
-        version: QrVersions.auto,
-        errorCorrectionLevel: QrErrorCorrectLevel.L);
-    //Validating the QR Code
-    QrCode? wemoQrCode;
-    if (qrValidationResult.status == QrValidationStatus.valid) {
-      wemoQrCode = qrValidationResult.qrCode;
-      //Painting the QR Image
-      final painter = QrPainter.withQr(
-        qr: wemoQrCode!,
-        color: kPurple,
-        gapless: false,
-        eyeStyle: const QrEyeStyle(eyeShape: QrEyeShape.circle),
-        dataModuleStyle:
-            const QrDataModuleStyle(dataModuleShape: QrDataModuleShape.circle),
-      );
-
-      ///Returning the picture data as a Uint8List
-
-      final picture =
-          await painter.toImageData(2048, format: ImageByteFormat.png);
-
-      final Uint8List pictureUint8List = picture!.buffer.asUint8List();
-      // await writeToFile(picData, imagePath);
-      return pictureUint8List;
-    } else {
-      qrValidationResult.error;
-    }
-    return null;
-  }
-
-  // Future<void> writeToFile(ByteData? data, String filePath) async {
+ // Future<void> writeToFile(ByteData? data, String filePath) async {
   //   final buffer = data!.buffer;
   //   await File(filePath).writeAsBytes(
   //       buffer.asUint8List(data.offsetInBytes, data.lengthInBytes));
   // }
+
+  
+  Future<Uint8List?> createQRImage(String data) async {
+ 
+  final qrValidationResult = QrValidator.validate(
+      data: data,
+      version: QrVersions.auto,
+      errorCorrectionLevel: QrErrorCorrectLevel.L);
+
+  QrCode? wemoQrCode;
+ 
+
+  if (qrValidationResult.status == QrValidationStatus.valid) {
+ wemoQrCode = qrValidationResult.qrCode;
+     final painter = QrPainter.withQr(
+    qr: wemoQrCode!,
+    color: kPurple,
+    gapless: false,
+    eyeStyle: const QrEyeStyle(eyeShape: QrEyeShape.circle),
+    dataModuleStyle:
+        const QrDataModuleStyle(dataModuleShape: QrDataModuleShape.circle),
+  );
+
+      ///Returning the picture data as a Uint8List
+      
+       final picture =
+          await painter.toImageData(2048);
+
+      
+   final Uint8List pictureUint8List = picture!.buffer.asUint8List();
+ return pictureUint8List;
+
+
+   }else{
+     qrValidationResult.error;
+   }
+
+  
+      // Map<String, String> imageinfo = {
+      //   "qrData": data,
+      //   "imgSize": "2048",
+      // };
+      // final picture =
+      //     await compute<Map<String, String>, ByteData?>(buildImage, imageinfo);
+
+     
+
+   
+    return null;
+     
+   
+  }
+
 }
+
+
+
+
