f396848562a43a3d10c01243d715a318125a8262
nuilewis <lewisnuikweh@gmail.com>
2022-08-06 12:10:58 +0100

    removed unused imports

--- lib/services/pdf_service.dart
+++ lib/services/pdf_service.dart
@@ -1,7 +1,5 @@
 import 'dart:io';
-import 'dart:isolate';
 import 'dart:typed_data';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
